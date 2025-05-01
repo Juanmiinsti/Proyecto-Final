c822d84bfa3273cd4049b971815f6b6f811fe352
Juan Miguel Ordoñez <juanmipineda02@gmail.com>
2025-05-01 13:57:44 +0200

    QuerysStatsApp

--- Database/proyectoFinal/src/main/java/com/example/proyectoFinal/repository/IMatchRepository.java
+++ Database/proyectoFinal/src/main/java/com/example/proyectoFinal/repository/IMatchRepository.java
@@ -2,9 +2,14 @@ package com.example.proyectoFinal.repository;
 
 import com.example.proyectoFinal.Entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IMatchRepository extends JpaRepository<Match, Integer> {
+    @Query("select m from  Match m join User u where u.name=?1")
+    List<Match>matchsbyUsername(String username);
 
 }
