dee3b62c938068c65badd00e13a326b99fe3b687
Juan Miguel Ordoñez <juanmipineda02@gmail.com>
2025-05-14 05:25:00 +0200

    DeleteItems

--- AppFightball/app/src/main/java/com/example/fightball/Admin/AdminItemsActivity.java
+++ AppFightball/app/src/main/java/com/example/fightball/Admin/AdminItemsActivity.java
@@ -1,6 +1,7 @@
 package com.example.fightball.Admin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -17,15 +18,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.fightball.API.RetroFitBuilder;
 import com.example.fightball.Adapters.AdminItemAdapter;
 import com.example.fightball.Models.ItemModelAdmin;
 import com.example.fightball.Models.UserModel;
 import com.example.fightball.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AdminItemsActivity extends AppCompatActivity {
     public static AdminItemAdapter adapter;
     ListView itemsVisual;
     Button create;
+    RetroFitBuilder retroFitBuilder = RetroFitBuilder.getInstance();
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,9 @@ public class AdminItemsActivity extends AppCompatActivity {
     }
 
     private void config() {
+
+        sp = getSharedPreferences("FightBall", MODE_PRIVATE);
+
         listofItems();
         create=findViewById(R.id.crearItemButton);
 
@@ -70,9 +81,10 @@ public class AdminItemsActivity extends AppCompatActivity {
                 Log.e("ContextMenu", "Usuario seleccionado es nulo");
                 return super.onContextItemSelected(item);
             }
-            if (selectedItem.getId()==R.id.IdEliminar){
+            if (item.getItemId()==R.id.IdEliminar){
+                deleteCall(selectedItem.getId(),info.position);
 
-            }else{
+            }else {
                 Intent intentEdit =new Intent(this,EditItemActivity.class);
                 intentEdit.putExtra("type",1);
                 intentEdit.putExtra("pos",info.position);
@@ -84,6 +96,28 @@ public class AdminItemsActivity extends AppCompatActivity {
         }
         return super.onContextItemSelected(item);
     }
+
+    private void deleteCall(int id,int pos) {
+        Call<Boolean>deleteItem=retroFitBuilder.callApi().deleteItem(sp.getString("key",""),id);
+        deleteItem.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.code()==200){
+                    AdminMainActivity.items.remove(pos);
+                    adapter.notifyDataSetChanged();
+                    System.out.println("cambios notificados");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                System.out.println("error al intentar Eliminar");
+
+            }
+        });
+
+    }
+
     private void listofItems(){
         adapter=new AdminItemAdapter(this,R.layout.admin_item_layout, AdminMainActivity.items);
         itemsVisual =findViewById(R.id.adminItemsLists);
